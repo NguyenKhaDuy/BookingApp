b75a941bf17214d22faf365458f772d071564518
NguyenKhaDuy <duynguyenst15@gmail.com>
2025-10-06 19:13:50 +0700

    update entity

---
+++ src/main/java/com/example/bookingapp/Entity/OtpVerificationEntity.java
@@ -0,0 +1,13 @@
+package com.example.bookingapp.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "otp_verifycation")
+public class OtpVerifycationEntity {
+}

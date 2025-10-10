565b84925c3659142c0c9c5c2fdcb671d6e63253
NguyenKhaDuy <duynguyenst15@gmail.com>
2025-10-10 18:56:48 +0700

    custom repo

--- src/main/java/com/example/bookingapp/Repository/TechnicianRepository.java
+++ src/main/java/com/example/bookingapp/Repository/TechnicianRepository.java
@@ -1,9 +1,12 @@
 package com.example.bookingapp.Repository;
 
 import com.example.bookingapp.Entity.TechnicianEntity;
+import com.example.bookingapp.Repository.Custom.TechnicianRepositoryCustom;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TechnicianRepository extends JpaRepository<TechnicianEntity, String> {
+public interface TechnicianRepository extends JpaRepository<TechnicianEntity, String>, TechnicianRepositoryCustom {
+
 }
